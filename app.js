f80bb1d715d620363cddd94973a69c118dd5f9ad
Ferhat ER <lersmooth@gmail.com>
2023-05-14 22:42:36 +0300

    ...

---
+++ app.js
@@ -0,0 +1,20 @@
+const productColors = [
+  { name: "light-blue" },
+  { name: "brown" },
+  { name: "purple" },
+];
+const colorElementContainer = document.querySelector(".color-options");
+
+colorElementContainer.innerHTML = productColors
+  .map((color, index) => {
+    return `<div class="color ${color.name}"></div>`;
+  })
+  .join("");
+
+const colorElements = document.querySelectorAll(".color");
+colorElements.forEach((colorElement, index) => {
+  colorElement.addEventListener("click", () => {
+    colorElement.classList.toggle("active");
+    document.body.style.setProperty("--primary", productColors[index].name);
+  });
+});
