f7292b8facb6cfeb26c0e7acb08ee475f0961157
Ferhat ER <lersmooth@gmail.com>
2023-05-15 14:32:15 +0300

    change functionality product image and its bgcolor

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
+// import colors & sytles from scss
 import colors from "./style.module.scss";
-const product = { name: "bag", colors: ["light-blue", "brown", "purple"] };
+
+//define product
+const product = {
+  name: "bag",
+  colors: ["light-blue", "purple", "brown"],
+};
+//define default color
+const defaultColor = "purple";
+
+//select product image
+const productImage = document.querySelector(".product-image img");
+
+//select color elements container
 const colorElementContainer = document.querySelector(".color-options");
 
+//set color elements container html content
 colorElementContainer.innerHTML = product.colors
   .map((color, index) => {
-    return `<div class="color ${color}"></div>`;
+    return `<div class="color ${
+      index == product.colors.indexOf(defaultColor) ? "active" : ""
+    }" style="background-color:${Object.values(colors)[index]};"></div>`;
   })
   .join("");
 
-const colorElements = document.querySelectorAll(".color");
-colorElements.forEach((colorElement, index) => {
+//select  color elements
+const colorSelectElements = document.querySelectorAll(".color");
+//set color for product image background
+colorSelectElements.forEach((colorElement, index) => {
+  //set default color as primary
+  document.body.style.setProperty(
+    "--primary",
+    Object.values(colors)[product.colors.indexOf(defaultColor)]
+  );
+  //set default product image by default color
+  productImage.src = `assets/img/women_bag_${defaultColor}.png`;
   colorElement.addEventListener("click", () => {
-    colorElement.classList.toggle("active");
+    //toggle active clas for colors
+    toggleActiveClass(colorElement, index);
+    //set default product image by selected color
+    productImage.src = `assets/img/women_bag_${Object.keys(colors)[index]}.png`;
+    //set selected color as primary
     document.body.style.setProperty("--primary", Object.values(colors)[index]);
   });
 });
+
+//toggle active class function
+function toggleActiveClass(element) {
+  colorSelectElements.forEach((item) => {
+    if (item != element) item.classList.remove("active");
+    element.classList.add("active");
+  });
+}
